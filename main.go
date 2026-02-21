1452524ae4a51f08f7a6d63b48567999c2f38986
magodo <wztwcy@gmail.com>
2026-02-21 11:47:59 +1100

    Add basic document generate directives in main.go

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/microsoft/terraform-provider-azuredevops/internal/provider"
 )
 
+//go:generate go tool tfplugindocs generate --provider-name azuredevops --rendered-provider-name AzureDevOps
 func main() {
 	var debug bool
 
